2eecaf3810d3c17577f5d3ff59c15bb5e7c934d7
Leonardo Goncalves Duarte da Silva (CodeBrain) <codebrain.leonardo.dasilva@kiwigrid.com>
2023-03-10 16:14:59 -0300

    Feitos alguns "pequenos" ajustes. A imagem do docker já está sendo publicada e está rodando corretamente.

---
+++ service/src/main/java/br/com/transportes/apitransportes/FullApiImplApplication.java
@@ -0,0 +1,13 @@
+package br.com.transportes.apitransportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullApiImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullApiImplApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/br/com/transportes/apitransportes/FullApiImplApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transportes.apitransportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullApiImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
