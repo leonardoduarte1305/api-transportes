649758cbb265ea07e4de3a217fe1f8d66bcffd7f
Leonardo Goncalves Duarte da Silva (CodeBrain) <codebrain.leonardo.dasilva@kiwigrid.com>
2023-03-18 18:45:27 -0300

    Implementado o ViagensController

---
+++ service/src/main/java/br/com/transportes/apitransportes/controller/ViagensController.java
@@ -0,0 +1,58 @@
+package br.com.transportes.apitransportes.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.transportes.apitransportes.service.ViagensService;
+import br.com.transportes.server.ViagensApi;
+import br.com.transportes.server.model.Confirmacao;
+import br.com.transportes.server.model.Destino;
+import br.com.transportes.server.model.UpsertViagem;
+import br.com.transportes.server.model.Viagem;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(value = "/viagens")
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class ViagensController implements ViagensApi {
+
+	private final ViagensService viagensService;
+
+	@PostMapping
+	@Override public ResponseEntity<Viagem> criarViagem(UpsertViagem upsertViagem) {
+		Viagem viagemSalva = viagensService.upsertViagem("", upsertViagem);
+		return ResponseEntity.ok(viagemSalva);
+	}
+
+	@PutMapping("/{id}")
+	@Override public ResponseEntity<Viagem> editarViagem(String id, UpsertViagem upsertViagem) {
+		Viagem viagemEditada = viagensService.upsertViagem(id, upsertViagem);
+		return ResponseEntity.ok(viagemEditada);
+	}
+
+	@PutMapping("/{id}/confirmacao")
+	@Override public ResponseEntity<Void> confirmaViagem(String id, Confirmacao confirmacao) {
+		viagensService.confirmaViagem(id, confirmacao);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping
+	@Override public ResponseEntity<List<Viagem>> listaTodasViagens() {
+		List<Viagem> encontradas = viagensService.listarViagens();
+		return ResponseEntity.ok(encontradas);
+	}
+
+	@GetMapping("/{id}/destinos")
+	@Override public ResponseEntity<List<Destino>> trazDestinosDaViagem(String id) {
+		List<Destino> destinosDaViagem = viagensService.listarDestinosDaViagem(id);
+		return ResponseEntity.ok(destinosDaViagem);
+	}
+}
