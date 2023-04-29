038b1735ca7916db25dc27407addf66681c96154
Leonardo Goncalves Duarte da Silva (CodeBrain) <codebrain.leonardo.dasilva@kiwigrid.com>
2023-04-29 14:10:13 -0300

    Remoção do security e das validações checkstyle

--- service/src/main/java/br/com/transportes/apitransportes/controller/DestinosController.java
+++ service/src/main/java/br/com/transportes/apitransportes/controller/DestinosController.java
@@ -3,7 +3,6 @@ package br.com.transportes.apitransportes.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,37 +23,31 @@ public class DestinosController implements DestinosApi {
 
 	private final DestinosService destinosService;
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<Destino> criarDestino(UpsertDestino upsertDestino) {
 		Destino destinoSalvo = destinosService.upsertDestino(null, upsertDestino);
 		return ResponseEntity.ok(destinoSalvo);
 	}
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<Destino> editarDestino(Integer id, UpsertDestino upsertDestino) {
 		Destino destinoEditado = destinosService.upsertDestino(id, upsertDestino);
 		return ResponseEntity.ok(destinoEditado);
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@Override public ResponseEntity<Void> excluirDestino(Integer id) {
 		destinosService.excluirDestinoPorId(id);
 		return ResponseEntity.noContent().build();
 	}
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<Destino> trazDestinoPorId(Integer id) {
 		Destino encontrado = destinosService.trazerDestinoPorId(id);
 		return ResponseEntity.ok(encontrado);
 	}
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<List<MaterialQuantidadeSetor>> trazMateriaisDoDestino(Integer id) {
 		List<MaterialQuantidadeSetor> materiais = destinosService.trazMateriaisDoDestino(id);
 		return ResponseEntity.ok(materiais);
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@Override public ResponseEntity<Void> confirmaDestino(Integer id, Confirmacao body) {
 		destinosService.confirmaDestino(id, body);
 		return ResponseEntity.noContent().build();
--- service/src/main/java/br/com/transportes/apitransportes/controller/ViagensController.java
+++ service/src/main/java/br/com/transportes/apitransportes/controller/ViagensController.java
@@ -6,7 +6,6 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,55 +27,46 @@ public class ViagensController implements ViagensApi {
 
 	private final ViagensService viagensService;
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<Viagem> criarViagem(UpsertViagem upsertViagem) {
 		Viagem viagemSalva = viagensService.upsertViagem(null, upsertViagem);
 		return ResponseEntity.ok(viagemSalva);
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@Override public ResponseEntity<Viagem> editarViagem(Integer id, UpsertViagem upsertViagem) {
 		Viagem viagemEditada = viagensService.upsertViagem(id, upsertViagem);
 		return ResponseEntity.ok(viagemEditada);
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@Override public ResponseEntity<Void> confirmaViagem(Integer id, Confirmacao confirmacao) {
 		viagensService.confirmaViagem(id, confirmacao);
 		return ResponseEntity.noContent().build();
 	}
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<List<Viagem>> listaTodasViagens() {
 		List<Viagem> encontradas = viagensService.listarViagens();
 		return ResponseEntity.ok(encontradas);
 	}
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<List<Destino>> trazDestinosDaViagem(Integer id) {
 		List<Destino> destinosDaViagem = viagensService.listarDestinosDaViagem(id);
 		return ResponseEntity.ok(destinosDaViagem);
 	}
 
-	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@Override public ResponseEntity<Viagem> trazViagemPorId(Integer id) {
 		Viagem encontrada = viagensService.trazerViagemPorId(id);
 		return ResponseEntity.ok(encontrada);
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@Override public ResponseEntity<Void> encerraViagem(Integer id, Encerramento encerramento) {
 		viagensService.encerraViagem(id, encerramento);
 		return ResponseEntity.noContent().build();
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@Override public ResponseEntity<Void> excluirViagem(Integer id) {
 		viagensService.excluirViagem(id);
 		return ResponseEntity.noContent().build();
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@Override public ResponseEntity<Object> relatorioDeViagem(Integer id) {
 		InputStreamResource inputStreamResource = viagensService.relatorioDeViagem(id);
 
