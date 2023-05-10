ab693aab1f0f38e147d8978c5e3b020ec808a4e4
Duarte <codebrain.leonardo.dasilva@kiwigrid.com>
2023-05-10 20:35:09 -0300

    Implementando a Criação de setores e a Listagem de setores.
    (cherry picked from commit 857e7e51a7735918f9e005b62cdd161c006aa69a)

--- service/src/main/java/br/com/transportes/apitransportes/service/SetoresService.java
+++ service/src/main/java/br/com/transportes/apitransportes/service/SetoresService.java
@@ -1,9 +1,14 @@
 package br.com.transportes.apitransportes.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import br.com.transportes.apitransportes.exception.EntidadeNaoEncontradaException;
+import br.com.transportes.apitransportes.mapper.SetoresMapper;
 import br.com.transportes.apitransportes.repository.SetoresRepository;
+import br.com.transportes.server.model.Setor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,11 +19,22 @@ public class SetoresService {
 
 	private final SetoresRepository setoresRepository;
 
+	private final SetoresMapper setoresMapper;
+
 	public br.com.transportes.apitransportes.entity.Setor encontrarSetorPorId(String id)
 			throws NumberFormatException {
 		Long idLong = Long.parseLong(id);
 		return setoresRepository.findById(idLong)
 				.orElseThrow(() -> new EntidadeNaoEncontradaException(
 						String.format("Setor com o id: %d não foi encontrado", idLong)));
+	}
+
+	public Setor criaSetor(Setor setor) {
+		return setoresMapper.toSetorDto(setoresRepository.save(setoresMapper.toSetorEntity(setor)));
+	}
+
+	public List<Setor> listaTodosOsSetores() {
+		List<br.com.transportes.apitransportes.entity.Setor> setores = setoresRepository.findAll();
+		return setores.stream().map(setoresMapper::toSetorDto).collect(Collectors.toList());
 	}
 }
